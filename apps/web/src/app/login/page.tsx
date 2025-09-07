073e4396b10b9fef7588acbdb0498969aca599ba
Tenuka22 <tenukaomaljith2009@gmail.com>
2025-09-07 17:40:34 +0530

    Refactor Login Page: Remove unused imports and session handling logic, simplifying the login component structure. Maintain focus on the sign-in and sign-up forms for improved clarity and user experience.

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,25 +1,10 @@
 'use client';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { SignInForm } from '@/components/auth/sign-in-form';
 import { SignUpForm } from '@/components/auth/sign-up-form';
-import LoadingSpinner from '@/components/global/page-loader';
-import { authClient } from '@/lib/auth-client';
 
 const LOGIN_PAGE = () => {
   const [showSignIn, setShowSignIn] = useState(false);
-  const { data: userSession, isPending } = authClient.useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (userSession?.user && !isPending) {
-      router.push('/');
-    }
-  }, [userSession, router, isPending]);
-
-  if (isPending) {
-    return <LoadingSpinner />;
-  }
 
   return showSignIn ? (
     <SignInForm onSwitchToSignUp={() => setShowSignIn(false)} />
